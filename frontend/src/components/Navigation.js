5ca36e17b11d33d64bff7e7c7cbeb48272549223
Paige Godfrey <paagodfrey@gmail.com>
2021-04-15 12:16:59 -0700

    jumbotron styling fixed

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -4,12 +4,14 @@ import "./Navigation.css";
 
 const Navigation = () => {
   return (
-    <header className="Navigation jumbotron mt-2">
-      <h1 className="display-4">Microblog</h1>
-      <p className="lead">Start blogging today!</p>
+    <header className="Navigation jumbotron mt-2 text-center">
+      <h1 className="display-3">Microblog</h1>
+      <p className="subheading lead">Start blogging today!</p>
       <nav>
-        <NavLink exact to="/">Home</NavLink>
-        <NavLink exact to="/new">Create Post</NavLink>
+        <div className="btn-group-justified">
+          <NavLink exact to="/" className="nav-btn btn btn-primary">Home</NavLink>
+          <NavLink exact to="/new" className="nav-btn btn btn-primary">Create Post</NavLink>
+        </div>
       </nav>
     </header>
   );
